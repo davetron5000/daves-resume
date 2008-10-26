b9e1aebb9151d1091d1d40fe9d6da35e945a7e61
David Copeland <davetron5000@gmail.com>
2008-10-26 18:14:19 -0400

    Moved more out of Rake, created command line tool for generating output

--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,11 +1,30 @@
+require 'erb'
 module Resume
+
+    # Handles formatting the resume using ERB templates.
+    # Create an instance of this class, then call a method named for your template.
+    # In +templates_dir+, if a file is named Xyz.erb, formatter.Xyz will run the resume through
+    # the template.  If an argument is passed to the method, the resulting file
+    # will be copied to the file named by the argument.
+    #
+    #     formatter = Formatter.new(resume,"MyResume","/home/davec/templates")
+    #     formatter.doc("DavesResume.doc")
+    #
+    # This will try to find +/home/davec/templates/doc.erb+ and use it to create your resume.  The
+    # resulting file would be +MyResume.doc+ and that file would be copied to +DavesResume.doc+
     class Formatter
+        # Create a new Formatter for a given resume
+        #
+        # +resume+:: The Resume object to format
+        # +resume_name+:: The basename of the files that will be output
+        # +templates_dir+:: location of your templates
         def initialize(resume,resume_name="resume",templates_dir="templates")
             @resume = resume
             @resume_name = resume_name
             @templates_dir = templates_dir
         end
 
+        # Does the formatting based on the method name, if the template is found.  Case matters
         def method_missing(method,*args)
             type = method.to_s
             template_file = "#{@templates_dir}/#{type}.erb"
